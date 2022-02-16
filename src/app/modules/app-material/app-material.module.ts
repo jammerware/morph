42f4d145e695043f57092af24179a5728ae712ce
Ben Stein <ben.s.stein@gmail.com>
2022-02-16 17:39:16 -0500

    Make pretty

--- src/app/modules/app-material/app-material.module.ts
+++ src/app/modules/app-material/app-material.module.ts
@@ -6,7 +6,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const MAT_MODULES = [
   MatButtonModule,
@@ -14,7 +14,7 @@ const MAT_MODULES = [
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  MatProgressBar,
+  MatProgressBarModule
 ]
 
 @NgModule({
