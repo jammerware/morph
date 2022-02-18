ac024621f479f169acfbe823d6ef2692495ed9c2
Ben Stein <ben.s.stein@gmail.com>
2022-02-18 02:33:21 -0500

    Work on character screen

--- src/app/models/character-decomposition.ts
+++ src/app/models/character-decomposition.ts
@@ -2,12 +2,15 @@ export interface ICharacterDecomposition {
     character: string;
     freqRank: number;
     pinyin: string;
-    semanticRadical: string;
     definitions: string[];
     strokeCount: number;
     isUnbound: boolean;
     commonWords: {
         word: string;
         frequency: number;
-    }[]
+    }[],
+    semanticRadical: {
+        radical: string;
+        definitions: string[] | null;
+    } | null
 }
