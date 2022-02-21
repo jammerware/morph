ab045e9dabaa105d10c0b235a987fbf72977955c
Ben Stein <ben.s.stein@gmail.com>
2022-02-21 10:26:13 -0500

    Make common words chips

--- src/app/modules/app-material/app-material.module.ts
+++ src/app/modules/app-material/app-material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -11,6 +12,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const MAT_MODULES = [
   MatButtonModule,
+  MatChipsModule,
   MatDialogModule,
   MatDividerModule,
   MatFormFieldModule,
