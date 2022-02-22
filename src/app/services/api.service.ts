d2ea808317c0f2cff5bf90f1635946e8bf97b5f7
Ben Stein <ben.s.stein@gmail.com>
2022-02-22 15:37:27 -0500

    Unbreak the word screen due to API changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -27,7 +27,10 @@ export class ApiService {
       throw new Error(`getDecomposition requires a non-null/non-empty query argument. Passed: "${query}"`)
     }
     
-    return this.http.post(`${this.API_ROOT}/decompose`, { word: query }).pipe(map(response => response as IDecomposition));
+    return this.http.post(`${this.API_ROOT}/decompose`, { word: query }).pipe(map((response: any) => {
+      response.translation = response.translation.translation;
+      return response as IDecomposition;
+    }));
     // return of({
     //   translation: "火山",
     //   characters: [
